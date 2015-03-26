c8113fce9a2384924a0dcb07df4ce3b52f79006f
Diego <diegofss11@gmail.com>
2015-03-26 16:10:41 -0300

    Include public/dist

---
+++ public/dist/js/templatesCache.js
@@ -0,0 +1,5 @@
+angular.module('bettingHost.tpls', []).run(['$templateCache', function($templateCache) {
+    'use strict';
+    $templateCache.put('partials/Dashboard.tpl.html',
+        '<div class=panel-heading><h2>Tote Betting</h2></div><div class="panel-body clearfix"><div class="input-container pull-left"><h3>Race input</h3><form name=inputForm ng-submit=vmDashboard.processResult()><textarea name=bets class=input-bets rows=20 cols=30 ng-model=vmDashboard.race.input required></textarea><button type=submit class="btn btn-primary btn-results" ng-disabled=inputForm.$invalid>Results</button></form></div><div class="pull-left output" ng-if=vmDashboard.race.output><h3>Race output</h3><pre class="alert alert-info" ng-bind=vmDashboard.race.output></pre></div><div class="pull-left output error" ng-if=vmDashboard.race.error><h3>Invalid input</h3><pre class="alert alert-danger" ng-bind=vmDashboard.race.error></pre></div></div><div class=panel-footer>Diego Souza - <a target=_blank href=https://github.com/diegofss11>GitHub</a></div>');
+}]);
